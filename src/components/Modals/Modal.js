5b3f95a38645818a8ad5cb8def96cd1eb989bf34
saania <saaniaaltaf121@gmail.com>
2024-08-30 04:19:22 +0500

    icon complete

--- src/components/Modals/Modal.js
+++ src/components/Modals/Modal.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Modal = ({ unsubscribeSubject, closeModal }) => { 
   return (
     <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center p-4">
-      <div className="bg-white p-6 rounded-lg shadow-md w-full max-w-sm sm:max-w-md">
+      <div className="bg-white p-6 shadow-md w-full max-w-sm sm:max-w-md">
         <div className="flex flex-col items-center mb-4">
           <div className="bg-red-500 text-white rounded-full w-12 h-12 flex items-center justify-center mb-2">
             <span className="text-3xl">!</span>
