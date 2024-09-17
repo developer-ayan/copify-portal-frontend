a45c9a1377d0a7ac42b3fbcea0cf93a9e01c4007
Developer-Ayan <58031124+developer-ayan@users.noreply.github.com>
2024-09-17 20:20:15 +0500

    * teacher almost done

--- src/utils/url.js
+++ src/utils/url.js
@@ -1,2 +1,2 @@
-export const base_url = `http://${'192.168.0.110'}:8000`;
+export const base_url = `http://${'192.168.0.111'}:8000`;
 export const image_base_url = "https://sassolution.org/lee/storage/app/public/";
